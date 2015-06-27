aad788973c06a2a6867cba1fa125a88932658649
BjornMelgaard <melgaardbjorn@gmail.com>
2015-06-27 13:43:39 +0300

    minor

--- llserver.py
+++ llserver.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-from scapy.all import *
 import sys
 import urllib
 import requests
 import simplejson as json
-from os import path, getcwd
+from os import path
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 from pprintpp import pprint as pp
-import socket
 
 FILE_PATH = path.join(path.expanduser('~'), 'anki.csv')
 IMAGE_DIR_PATH = path.join(path.expanduser('~'), 'Documents', 'Anki', 'User 1', 'collection.media')
@@ -15,86 +14,88 @@ JOIN_SYMBOL = '|'
 SUPPORT_HTML = True
 SAVE_PICTURES = True
 
-def handle(package):
-    #parsing package if it have data, else - exit from func
-    if not package.haslayer(Raw): return
-    raw = str(package.getlayer(Raw).load)
-    if "word" not in raw or not "context" in raw: return
-    s = raw.lstrip("b'").rstrip("'").split("&")  # delete b' on  and '
-    try:
-        request_word = s[0].split("=")[-1].strip()
-        context = s[2].split("=")[-1].strip()
-    except Exception as e:
-        print(e)
-        print('s =', s)
-        return
+class Handler(SimpleHTTPRequestHandler):
+    def do_POST(self):
+        self.send_cap()
+        data = {
+            "orig_word": None,
+            "word": None,
+            "type": None,
+            "translations": None,
+            "transcription": None,
+            "context": None,
+            "pic_name": None,
+            "sound_url": None #for future
+        }
 
-    data = {
-        "request_word": request_word,
-        "context": context,
-        "word": '',
-        "type": '',
-        "translations": '',
-        "transcription": '',
-        "pic_name": '',
-        "sound_url": ''  # for future
-    }
+        interception = self.get_interception()
+        self.send_cap()
+        data['orig_word'] = interception['word'][0]
+        data['context'] = interception['context_title'][0]
+        if (SUPPORT_HTML):
+            data['context'] = data['context'].replace(
+                data['request_word'], "<b>" + data['request_word'] + "</b>")
+        
+        orig_transl = get_word_info(request_word)
+        data['sound_url'] = orig_transl.get('sound_url', None)
+        
+        try:
+            transl = get_word_info(orig_transl['word_forms'][0]['word'])
+            pic_url = transl.get('pic_url', None)
+            pic_name = pic_url.split('/')[-1] if pic_url else ''
+            if (SAVE_PICTURES and pic_name):
+                dowload_pic(pic_url, pic_name)
+        
+            data['pic_name'] = pic_name
+            data['word'] = transl['word_forms'][0]['word']
+            data['type'] = transl['word_forms'][0]['type']
+            data['translations'] = ', '.join(
+                [t['value'] for t in transl['translate']])
+            data['transcription'] = transl['transcription']
+        except IndexError:
+            data['word'] = data['request_word']
+        
+        pp(data)
+        line = data['word'] + JOIN_SYMBOL + \
+            data['translations'] + JOIN_SYMBOL + \
+            data['transcription'] + JOIN_SYMBOL + \
+            data['pic_name'] + JOIN_SYMBOL + \
+            data['context'] + "\n"
+        with open(FILE_PATH, "a") as text_file:
+            text_file.write(line)
 
-    if (SUPPORT_HTML):
-        data['context'] = data['context'].replace(
-            data['request_word'], "<b>" + data['request_word'] + "</b>")
+    def get_interception(self):
+        rawbody = self.rfile.read(
+            int(self.headers['Content-Length'])).decode("utf-8")
+        return urllib.parse.parse_qs(rawbody)
 
-    orig_transl = get_word_info(request_word)
-    data['sound_url'] = orig_transl.get('sound_url', None)
+    def send_cap(self):
+        # TODO
+        # prevent error message in extention (not work)
+        pass
+        # self.send_response(200)
 
-    try:
-        transl = get_word_info(orig_transl['word_forms'][0]['word'])
-        pic_url = transl.get('pic_url', None)
-        pic_name = pic_url.split('/')[-1] if pic_url else ''
-        if (SAVE_PICTURES and pic_name):
-            dowload_pic(pic_url, pic_name)
+    def get_word_info(self, word, host="https://api.lingualeo.com/gettranslates", include_media=1, add_word_forms=1):
+        data = {
+            'word': word,
+            'include_media': include_media,
+            'add_word_forms': add_word_forms
+        }
+        return requests.post(host, data).json()
 
-        data['pic_name'] = pic_name
-        data['word'] = transl['word_forms'][0]['word']
-        data['type'] = transl['word_forms'][0]['type']
-        data['translations'] = ', '.join(
-            [t['value'] for t in transl['translate']])
-        data['transcription'] = transl['transcription']
-    except IndexError:
-        data['word'] = data['request_word']
-
-    pp(data)
-    line = data['word'] + JOIN_SYMBOL + \
-        data['translations'] + JOIN_SYMBOL + \
-        data['transcription'] + JOIN_SYMBOL + \
-        data['pic_name'] + JOIN_SYMBOL + \
-        data['context'] + "\n"
-    with open(FILE_PATH, "a") as text_file:
-        text_file.write(line)
-
-def get_word_info(word, host="https://api.lingualeo.com/gettranslates", include_media=1, add_word_forms=1):
-    data = {
-        'word': word,
-        'include_media': include_media,
-        'add_word_forms': add_word_forms
-    }
-    return requests.post(host, data).json()
-
-def dowload_pic(pic_url, pic_name):
-    dir_path = IMAGE_DIR_PATH
-    if (not pic_url):
-        raise ValueError("pic_url is empty")
-
-    pic_path = path.join(dir_path, pic_name)
-    if (path.isfile(pic_path)):
-        # there no need to rewrite the file
-        return
-
-    r = requests.get(pic_url, stream=True)
-    if r.status_code == 200:
-        with open(pic_path, 'wb') as f:
-            for chunk in r.iter_content():
-                f.write(chunk)
+    def dowload_pic(self, pic_url, pic_name):
+        dir_path = picDirPath
+        if (not pic_url):
+            raise ValueError("pic_url is empty")
+        pic_path = path.join(dir_path, pic_name)
+        if (path.isfile(pic_path)):
+            # there no need to rewrite the file
+            return
+        r = requests.get(pic_url, stream=True)
+        if r.status_code == 200:
+            with open(pic_path, 'wb') as f:
+                for chunk in r.iter_content():
+                    f.write(chunk)
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -111,6 +112,7 @@ if __name__ == '__main__':
                       help="don't save pictures in folder (Anki folder in default)")
     opts = parser.parse_args()
 
+    server_address = ('127.0.0.1', 3000)
     FILE_PATH = path.abspath(opts.file_path)
     IMAGE_DIR_PATH = path.abspath(opts.image_dir_path)
     JOIN_SYMBOL = opts.join_symbol
@@ -124,9 +126,13 @@ if __name__ == '__main__':
 
     print("Word data will be writen to %s" % FILE_PATH)
     print("Images will be saved to %s" % IMAGE_DIR_PATH)
-    print("Packages will be filtered by %s host" % HOST)
+
+    httpd = HTTPServer(server_address, Handler)
+    print('http server is running...listening on port %s' % server_address[1])
+
     try:
-        filter = "dst host " + socket.gethostbyname("api.lingualeo.com")
-        sniff(iface="wlp3s0", filter=filter, prn=handle)
+        httpd.serve_forever()
     except KeyboardInterrupt:
         sys.exit(0)
+    finally:
+        httpd.server_close()

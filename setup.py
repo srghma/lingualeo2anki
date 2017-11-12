3f077f6afdce9578868e2e9b26149cfa10d5458f
BjornMelgaard <melgaardbjorn@gmail.com>
2017-11-12 19:17:26 +0200

    mvp!

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 from setuptools import setup
+import os
 
 setup(
-    test_suite="tests"
+    name="lingualeo2anki",
+    version="0.0.1",
+    author='Sergey Homa',
+    author_email="melgaardbjorn@gmail.com",
+    packages=[
+        'server',
+    ],
+    entry_points={
+        "console_scripts": [
+            "lingualeo2anki=server.__main__:main"
+        ]
+    },
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+    ],
+    description='Hack lingualeo chrome plugin and save dictionary locally, ready for anki import',
 )
+
